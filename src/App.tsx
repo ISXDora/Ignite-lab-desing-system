010d630315a6696cf18903c98a91b91a6e84ee70
ISXDora <isinhasena@gmail.com>
2022-10-13 18:11:01 -0300

    create component text with radix & config tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Text } from './components/Text'
 import './styles/global.css'
 export function App() {
 
   return (
-   <h1>Desing System React</h1>
+    <>
+   <h1 className='text-2xl'>Desing System React</h1>
+   <Text size='md'>Ola React</Text>
+    </>
   )
 }
 
