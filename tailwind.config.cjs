a55fd9fdc3737171968cabf71c1f3070a022c18a
ISXDora <isinhasena@gmail.com>
2022-10-13 22:54:02 -0300

    estilizacao e doc storybook do comp Button

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,6 +13,8 @@ module.exports = {
       '2xl': 32,
     },
     colors: {
+      'black': '#000000',
+      'white': '#ffffff',
       'gray-900': '#121214',
       'gray-800': '#202024',
       'gray-400': '#7c7c8a',
@@ -21,7 +23,7 @@ module.exports = {
 
 
       'cyan-500': '#81d8f7',
-      'gray-800': '#AAE9FF',    
+      'cyan-300': '#AAE9FF',    
 
     },
     extend: {
